2e55126f9fb37eb566a98a4e73ac5d29a4fad830
Sneha Parihar <sneha.parihar@publicissapient.com>
2020-10-05 00:31:10 +0200

    Added tests for quicklook endpoint

--- feature-search-application/src/test/kotlin/com/assignment/search/application/FeaturesSearchApplicationIntegrationTest.kt
+++ feature-search-application/src/test/kotlin/com/assignment/search/application/FeaturesSearchApplicationIntegrationTest.kt
@@ -8,15 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.core.ParameterizedTypeReference
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
 import java.time.LocalDate
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
-
 
     @Test
     fun `Assert getAll features endpoint`() {
@@ -54,6 +51,32 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
         val str = "not-existing-value"
         val responseEntity: ResponseEntity<ErrorsDetails> = restTemplate.getForEntity<ErrorsDetails>("/features/$str", ErrorsDetails::class.java)
+        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+        assertThat(responseEntity.body).isEqualTo(ErrorsDetails(time = LocalDate.now(), message = "Feature Not " +
+                "Found!", details = "FeatureId: $str"))
+
+    }
+
+    @Test
+    fun `Assert getQuicklook features endpoint`() {
+        val requestHeaders = HttpHeaders().also {
+            it.add(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_PNG.toString())
+        }
+        val entity = HttpEntity<String>(requestHeaders)
+
+        val str = "08a190bf-8c7e-4e94-a22c-7f3be11f642c"
+        val responseEntity: ResponseEntity<ByteArray> =
+                restTemplate.exchange("/features/$str/quicklook", HttpMethod.GET,
+                        entity, object : ParameterizedTypeReference<ByteArray>() {})
+
+        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
+    }
+
+    @Test
+    fun `Assert getQuicklook features endpoint Exception Handling`() {
+
+        val str = "not-existing-value"
+        val responseEntity: ResponseEntity<ErrorsDetails> = restTemplate.getForEntity<ErrorsDetails>("/features/$str/quicklook", ErrorsDetails::class.java)
         assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
         assertThat(responseEntity.body).isEqualTo(ErrorsDetails(time = LocalDate.now(), message = "Feature Not " +
                 "Found!", details = "FeatureId: $str"))
