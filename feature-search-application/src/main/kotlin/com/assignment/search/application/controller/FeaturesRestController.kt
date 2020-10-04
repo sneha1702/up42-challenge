be156d9e153f2824c006504316a6c1a19ea8c91b
Sneha Parihar <sneha.parihar@publicissapient.com>
2020-10-04 22:03:15 +0200

    Added ControllerAdvice for exception handling

--- feature-search-application/src/main/kotlin/com/assignment/search/application/controller/FeaturesRestController.kt
+++ feature-search-application/src/main/kotlin/com/assignment/search/application/controller/FeaturesRestController.kt
@@ -1,14 +1,15 @@
 package com.assignment.search.application.controller
 
 import com.assignment.search.application.service.FeaturesService
+import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
 import org.apache.commons.io.IOUtils
 import org.springframework.http.*
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import java.io.InputStream
 import java.nio.charset.StandardCharsets
+import java.time.LocalDate
 
 
 @RestController
@@ -17,5 +18,30 @@ class FeaturesRestController(val featureService: FeaturesService) {
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     fun features() = featureService.getAll()
 
-
 }
+
+@ControllerAdvice
+class ControllerAdviceRequestError : ResponseEntityExceptionHandler() {
+    @ExceptionHandler(value = [(FeaturesNotFoundException::class)])
+    fun handleUserAlreadyExists(ex: FeaturesNotFoundException, request: WebRequest): ResponseEntity<ErrorsDetails> {
+        val errorDetails = ErrorsDetails(LocalDate.now(),
+                "Validation Failed",
+                ex.message!!
+        )
+        return ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(MissingKotlinParameterException::class)
+    @ResponseBody
+    fun handleMissingParameterError(ex: MissingKotlinParameterException): ResponseEntity<ErrorsDetails> {
+        val errorDetails = ErrorsDetails(LocalDate.now(),
+                "Parsing of Json to Java Object Failed!", ex.message!!
+        )
+        return ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST)
+    }
+}
+
+data class ErrorsDetails(val time: LocalDate, val message: String, val details: String)
+
+class FeaturesNotFoundException(override val message: String?) : Exception(message)
+
