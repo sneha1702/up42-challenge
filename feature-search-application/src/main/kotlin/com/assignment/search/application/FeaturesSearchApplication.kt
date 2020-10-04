525ebf9f2275603c152c1023455a10fc37f98817
Sneha Parihar <sneha.parihar@publicissapient.com>
2020-10-04 20:19:19 +0200

    Added FeaturesSearchApplication.kt spring boot kotlin application

---
+++ feature-search-application/src/main/kotlin/com/assignment/search/application/FeaturesSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.assignment.search.application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeaturesSearchApplication
+
+fun main(args: Array<String>) {
+    runApplication<FeaturesSearchApplication>(*args)
+}
