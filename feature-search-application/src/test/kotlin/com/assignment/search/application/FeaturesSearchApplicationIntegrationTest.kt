7350169cab1aa5b435b381d7d973654bf6dcbf09
Sneha Parihar <sneha.parihar@publicissapient.com>
2020-10-04 22:01:58 +0200

    Added Integration test to verify get all features endpoint

--- feature-search-application/src/test/kotlin/com/assignment/search/application/FeaturesSearchApplicationIntegrationTest.kt
+++ feature-search-application/src/test/kotlin/com/assignment/search/application/FeaturesSearchApplicationIntegrationTest.kt
@@ -6,15 +6,11 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import java.awt.Menu
 
 
-@Suppress("UNCHECKED_CAST")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
@@ -26,7 +22,7 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
         assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
         val result: List<ResultFeaturesData> = responseEntity.body!!
         assertThat(result.size).isNotEqualTo(0)
-        val firstData = result.stream().findFirst().get();
+        val firstData = result.stream().findFirst().get()
         assertThat(firstData.id).isEqualTo("08a190bf-8c7e-4e94-a22c-7f3be11f642c")
         assertThat(firstData.beginViewingDate).isEqualTo("1555044772083")
         assertThat(firstData.endViewingDate).isEqualTo("1555044797082")
